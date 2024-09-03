282ed8636442640ae16cb2e331c031c4139e6913
Kaleem Mahesar <kaleemmahesar@hotmail.com>
2024-09-03 16:52:00 +0500

    pushing after fixing issue

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import ProductList from "./_components/ProductList";
 import Banner from "./_components/Banner";
 
 export default async function Home() {
-	const sliderList = await GlobalApi.getSlides()
-	const productList = await GlobalApi.getProducts()
+	// const sliderList = await GlobalApi.getSlides()
+	// const productList = await GlobalApi.getProducts()
 	return (
 		<div className="p-10 px-16">
-			<Slider sliderList={sliderList} />
+			{/* <Slider sliderList={sliderList} />
 			<ProductList productList={productList} />
-			<Banner />
+			<Banner /> */}
 		</div>
 	);
 }
